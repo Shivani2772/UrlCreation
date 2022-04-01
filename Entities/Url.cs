56cc100f0556fa33ef19c29d9bd811f805603d01
Shivani <shivani.patel@campus-support.ca>
2022-03-31 17:30:31 -0700

    Add project files.

---
+++ Entities/Url.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrlCreation.Entities
+{
+    /// <summary>
+    /// Url entity.
+    /// </summary>
+    public class Url
+    {
+        /// <summary>
+        /// Initialize the url.
+        /// </summary>
+        /// <param name="shortUrl">Short url.</param>
+        /// <param name="longUrl">long url.</param>
+       
+        public Url(string shortUrl, string longUrl)
+        {
+            this.ShortUrl = shortUrl;
+            this.LongUrl = longUrl;
+        }
+
+        /// <summary>
+        /// Ef core initialization constructor.
+        /// </summary>
+        protected Url()
+        {
+        }
+
+
+        /// <summary>
+        /// Gets the primary key of Url.
+        /// </summary>
+        public long id { get; private set; }
+
+        /// <summary>
+        /// Gets the unique code for url.
+        /// </summary>
+        public string Code { get; private set; }
+
+        /// <summary>
+        /// Gets the short url.
+        /// </summary>
+        public string ShortUrl { get; private set; }
+
+        /// <summary>
+        /// Gets the long url.
+        /// </summary>
+        public string LongUrl { get; private set; }
+    }
+}
